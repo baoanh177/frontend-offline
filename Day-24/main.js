63510f1311c12ff0d22bce309adcb99faed3e137
Baoanh2004 <baoanh27042004@gmail.com>
2023-09-11 23:51:44 +0700

    Bo sung bai 2

--- Day-24/main.js
+++ Day-24/main.js
@@ -10,51 +10,59 @@ console.groupEnd()
 // Bai 2
 console.group('Bai 2');
 ((number) => {
-    // if(Number.isInteger(+number)) {
-    //     const units = ['', 'một', 'hai', 'ba', 'bốn', 'năm', 'sáu', 'bảy', 'tám', 'chín']
-    //     const sub = [' nghìn ', ' triệu ', ' tỉ ']
+    if(Number.isInteger(+number)) {
+        const units = ['', 'một', 'hai', 'ba', 'bốn', 'năm', 'sáu', 'bảy', 'tám', 'chín']
+        const sub = [' nghìn ', ' triệu ', ' tỉ ']
 
-    //     const handleDozens = (first, sec, last) => {
-    //         if(first == 0 && sec == 0 && last == 0) {
-    //             return ''
-    //         }else if(sec == 0 && last == 0) {
-    //             return `${first == 0 ? 'không' : `${units[first]}`} trăm linh ${units[last]}`
-    //         }else {
-    //             return `${first == 0 ? 'không' : `${units[first]}`} trăm linh ${units[last]}`
-    //         }
-    //     }
+        const handleThreeSo = (first, sec, last) => {
+            if(first == 0 && sec == 0 && last == 0) {
+                return ''
+            }else if(sec == 0) {
+                return `${first == 0 ? 'không' : units[first]} trăm ${last == 0 ? '' : `linh ${units[last]}`}`
+            }else if(last == 0) {
+                return `${first == 0 ? 'không' : units[first]} trăm ${sec == 1 ? `mười` : `${units[sec]} mươi`}`
+            }else if(sec == 1) {
+                return `${first == 0 ? 'không' : units[first]} trăm mười ${units[last]}`
+            } else {
+                return `${first == 0 ? 'không' : units[first]} trăm ${units[sec]} ${last == 1 ? 'mốt' : `${units[last]}`}`
+            }
+        }
 
-    //     const ingre = number.toLocaleString('vi-VN').split('.')
+        const handleTwoSo = (first, sec) => {
+            if(first == 1 && sec == 1) {
+                return 'mười một'
+            }else if(first = 1) {
+                return `mười ${units[sec]}`
+            }else if(sec == 1) {
+                return `${units[first]} mươi mốt`
+            }else {
+                return `${units[first]} mươi ${sec == 0 ? '' : units[sec]}`
+            }
+        }
 
+        const ingre = number.toLocaleString('vi-VN').split('.')
 
-    //     const result = ingre.map(num => {
-    //         if(num.length == 3) {
-    //             if(num[0] != 0) {
-    //                 return `${num[0] == 0 ? 'không' : units[num[0]]} trăm ` + // Hàng trăm
-    //                 `${num[1] == 1 ? 'mười ' : `${units[num[1]]} mươi `}` + // Hàng chục
-    //                 `${num[2] == 1 ? 'mốt' : units[num[2]]}`
-    //             }
-    //         }else if(num.length == 2) {
-    //             return `${num[0] > 1 ? `${units[num[0]]} mươi` : 'mười'} ${num[1] == 1 ? 'mốt' : units[num[1]]}`
-    //         }else {
-    //             return units[num[0]]
-    //         }
-    //     })
+        const result = ingre.map(num => {
+            if(num.length == 3) {
+                return handleThreeSo(num[0], num[1], num[2])
+            }else if(num.length == 2) {
+                return handleTwoSo(num[0], num[1])
+            }else {
+                return num[0] != 0 ? units[num[0]] : 'không'
+            }
+        })
 
-    //     console.log(result)
-
-    //     let count = 0
-    //     for(let i = result.length - 1; i > 0; i--) {
-    //         if(count == 3) {
-    //             count = 0
-    //         }
-    //         result.splice(i, 0, sub[count])
-    //         count++
-    //     }
-    //     console.log(result.join(''))
-    // }
-    console.log('Do có việc đột xuất nên chưa thể hoàn thiện. Em sẽ bổ sung vào đêm nay')
-})(1000000000)
+        let count = 0
+        for(let i = result.length - 1; i > 0; i--) {
+            if(count == 3) {
+                count = 0
+            }
+            result.splice(i, 0, sub[count])
+            count++
+        }
+        console.log(result.join(''))
+    }
+})(213131423)
 console.groupEnd()
 
 // Bai 3
