601fc89449c501da5a8fd1c91149242e76fab633
Baoanh2004 <baoanh27042004@gmail.com>
2023-10-05 00:49:07 +0700

    Nop bai ngay 31

---
+++ Day-31/main.js
@@ -0,0 +1,47 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const inner = $('.inner')
+const items = $$('li')
+
+items.forEach((item) => {
+    let elementOffsetY = 0
+    
+    const getElementOffsetY = e => {
+        elementOffsetY = e.offsetY // Từ vị trí nhấn chuột đến top của element
+        e.target.style.filter = "hue-rotate(40deg)"
+        const handleDragEnd = () => {
+            e.target.style.filter = ""
+            handleArrange()
+            e.target.removeEventListener('dragend', handleDragEnd)
+        }
+        e.target.addEventListener('dragend', handleDragEnd)
+    }
+
+    item.addEventListener('dragstart', getElementOffsetY)
+    const handleDrag = e => {
+        let prev = next = false
+        const nextElement = e.target.nextElementSibling
+        let prevElement = e.target.previousElementSibling
+        const elementPositionTop = e.clientY - elementOffsetY // Điểm trên cùng của item được kéo
+
+        if(nextElement && elementPositionTop > nextElement.offsetTop + 10) { // + 10: kéo quá khoảng 10px thì thay đổi vị trí
+            inner.insertBefore(item, nextElement.nextElementSibling)
+        }else if(elementPositionTop > elementOffsetY && prevElement && elementPositionTop <= prevElement.offsetTop - 10) { // e k biết giait thích sao đâu =))
+            inner.insertBefore(item, item.previousElementSibling)
+        }
+    }
+    item.addEventListener('drag', handleDrag)
+})
+
+const handleArrange = () => {
+    $$('.module').forEach((item, index) => {
+        item.querySelector('span').innerText = index + 1
+    })
+
+    $$('.lesson').forEach((item, index) => {
+        item.querySelector('span').innerText = index + 1
+    })
+}
+
+handleArrange()
