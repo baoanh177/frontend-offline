10b131350970975fe24b2eaf2deb92e080703967
= <=>
2023-11-25 01:55:18 +0700

    update

--- Day-45/src/Error.js
+++ Day-45/src/Error.js
@@ -1,6 +1,7 @@
 import image from "./assets/images/404-page-best-practice.png.webp"
 
 function Error() {
+    console.log('test')
     return `<img src="${image}" alt="" style="display: block; margin: auto; width: 95%; height: 80%;"/>`;
 }
 
