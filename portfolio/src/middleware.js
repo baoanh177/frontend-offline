54aa71f842419cf7756ccc37915c24990915d7ef
baoanh <=>
2024-01-10 13:08:50 +0700

    update

--- portfolio/src/middleware.js
+++ portfolio/src/middleware.js
@@ -1,33 +1,19 @@
 import { NextResponse } from "next/server"
-import { getToken } from "next-auth/jwt"
-const secret = process.env.AUTH_SECRET
+// import { getToken } from "next-auth/jwt"
+// const secret = process.env.AUTH_SECRET
 
 const middleware = async request => {
     const pathname = request.nextUrl.pathname
     const lang = request.cookies.get('lang')?.value || 'vi'
-    // const token = request.cookies.get('next-auth.csrf-token')?.value
-    // // console.log(token)
-
-    // if(!token) {
-    //     return NextResponse.redirect(`${request.url}${lang}`)
-    // }
-
-    // console.log(request)
-
-    const jwt = await getToken({
-        req: request
-    })
-
-    console.log('jwttttttttttttttttt:', jwt)
-
+    
     if(pathname == '/') {
         return NextResponse.redirect(`${request.url}${lang}`)
     }
-    if(pathname.startsWith('/vi/profile') || pathname.startsWith('/en/profile')) {
-        // console.log('pathname:', pathname)
-        // const url = new URL('/vi/', request.url)
-        // return NextResponse.redirect(url)
-    }
+    // if(pathname.startsWith('/vi/profile') || pathname.startsWith('/en/profile')) {
+    //     // console.log('pathname:', pathname)
+    //     // const url = new URL('/vi/', request.url)
+    //     // return NextResponse.redirect(url)
+    // }
 
 }
 
